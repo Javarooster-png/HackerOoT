43aafba06508dad2160744acf706cfa62d3a7378
Javarooster-png <9499212+Javarooster-png@users.noreply.github.com>
2026-05-17 19:54:04 -0500

    Fog Actor

---
+++ src/overlays/actors/ovl_Bg_Weather/z_bg_weather.h
@@ -0,0 +1,19 @@
+#ifndef Z_BG_WEATHER_H
+#define Z_BG_WEATHER_H
+
+#include "ultra64.h"
+#include "actor.h"
+
+typedef struct BgWorld {
+    /* 0x0000 */ Actor actor;
+    /* 0x014C */ f32 rainFogIntensity;
+    /* 0x0150 */ f32 nightMistIntensity;
+    /* 0x0154 */ Vec3f fogLastEye;
+    /* 0x0160 */ f32 fogWakeX;
+    /* 0x0164 */ f32 fogWakeZ;
+    /* 0x0168 */ u8 fogWakeInitialized;
+    /* 0x016A */ u16 localDayTime;
+    /* 0x016C */ f32 basePlayerY;
+} BgWorld; // size = 0x0170
+
+#endif
